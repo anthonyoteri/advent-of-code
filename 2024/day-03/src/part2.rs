3d550462e6f7c1b6ac6dbb6f258006f7332d7e7c
Anthony Oteri <4360016+anthonyoteri@users.noreply.github.com>
2024-12-03 11:41:49 -0500

    feat(day3): part 2

--- 2024/day-03/src/part2.rs
+++ 2024/day-03/src/part2.rs
@@ -1,8 +1,38 @@
 use crate::error::AocError;
+use regex::Regex;
 
 #[tracing::instrument]
 pub fn process(input: &str) -> miette::Result<u64, AocError> {
-    Ok(0)
+    let pairs = parse(input);
+
+    let result = pairs.iter().map(|(x, y)| *x * *y).sum::<u64>();
+    Ok(result)
+}
+
+#[tracing::instrument(ret)]
+fn parse(input: &str) -> Vec<(u64, u64)> {
+    let input = input.replace("\n", "");
+
+    let re = Regex::new(r"(^|do\(\))(.*?)(don\'t\(\)|$)").unwrap();
+    re.captures_iter(&input)
+        .map(|group| {
+            let re = Regex::new(r"(mul\(\d+,\d+\))").unwrap();
+            re.captures_iter(group.get(2).unwrap().as_str())
+                .map(|cap| {
+                    let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+                    match re.captures(cap.get(0).unwrap().as_str()) {
+                        Some(caps) => {
+                            let x = caps.get(1).unwrap().as_str().parse::<u64>().unwrap();
+                            let y = caps.get(2).unwrap().as_str().parse::<u64>().unwrap();
+                            (x, y)
+                        }
+                        None => (0, 0),
+                    }
+                })
+                .collect::<Vec<(u64, u64)>>()
+        })
+        .flatten()
+        .collect()
 }
 
 #[cfg(test)]
@@ -11,8 +41,9 @@ mod tests {
 
     #[test_log::test]
     fn test_process() -> miette::Result<()> {
-        let input = include_str!("../test-input.txt");
-        assert_eq!(0, process(input)?);
+        let input =
+            "xmul(2,4)mul(2,2)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
+        assert_eq!(52, process(input)?);
         Ok(())
     }
 }
