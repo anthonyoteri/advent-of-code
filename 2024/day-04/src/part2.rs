3a02d70dd1873c09f71f23aad8bf2b1cfc3544f0
Anthony Oteri <4360016+anthonyoteri@users.noreply.github.com>
2024-12-04 11:36:45 -0500

    feat(day4): part 2

--- 2024/day-04/src/part2.rs
+++ 2024/day-04/src/part2.rs
@@ -1,8 +1,93 @@
-use crate::error::AocError;
+use core::panic;
 
+use crate::{error::AocError, grid::neighbors};
+use glam::IVec2;
+use nom::{
+    character,
+    multi::{self, separated_list1},
+    IResult,
+};
+
+use crate::grid::{boundaries, filter_values, next_point, rows, Direction, Grid};
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+enum Tile {
+    X,
+    M,
+    A,
+    S,
+}
+
+impl From<char> for Tile {
+    fn from(c: char) -> Self {
+        match c {
+            'X' => Self::X,
+            'M' => Self::M,
+            'A' => Self::A,
+            'S' => Self::S,
+            _ => panic!("Invalid letter"),
+        }
+    }
+}
+
+fn parse_tile(input: &str) -> IResult<&str, Tile> {
+    character::complete::one_of("XMAS")(input).map(|(input, c)| (input, Tile::from(c)))
+}
+
+fn parse(input: &str) -> IResult<&str, Grid<Tile>> {
+    let (input, rows) =
+        separated_list1(character::complete::newline, multi::many1(parse_tile))(input)?;
+
+    let grid = rows
+        .into_iter()
+        .enumerate()
+        .flat_map(|(y, row)| {
+            row.into_iter().enumerate().map(move |(x, tile)| {
+                let position = IVec2::new(x as i32, y as i32);
+                (position, tile)
+            })
+        })
+        .collect::<Grid<Tile>>();
+
+    Ok((input, grid))
+}
+
+fn search(grid: &Grid<Tile>) -> Vec<IVec2> {
+    let starting_positions = filter_values(grid, |tile| *tile == Tile::A);
+
+    starting_positions
+        .keys()
+        .filter_map(|start| {
+            let ne_neighbor = next_point(&start, &Direction::NorthEast);
+            let se_neighbor = next_point(&start, &Direction::SouthEast);
+            let sw_neighbor = next_point(&start, &Direction::SouthWest);
+            let nw_neighbor = next_point(&start, &Direction::NorthWest);
+
+            let ne_tile = grid.get(&ne_neighbor);
+            let se_tile = grid.get(&se_neighbor);
+            let sw_tile = grid.get(&sw_neighbor);
+            let nw_tile = grid.get(&nw_neighbor);
+
+            match (ne_tile, se_tile, sw_tile, nw_tile) {
+                (Some(Tile::M), Some(Tile::M), Some(Tile::S), Some(Tile::S))
+                | (Some(Tile::S), Some(Tile::S), Some(Tile::M), Some(Tile::M))
+                | (Some(Tile::M), Some(Tile::S), Some(Tile::S), Some(Tile::M))
+                | (Some(Tile::S), Some(Tile::M), Some(Tile::M), Some(Tile::S)) => {
+                    Some(start.clone())
+                }
+                _ => None,
+            }
+        })
+        .collect()
+}
 #[tracing::instrument]
 pub fn process(input: &str) -> miette::Result<u64, AocError> {
-    Ok(0)
+    let (input, grid) = parse(input).unwrap();
+    debug_assert!(input.is_empty());
+
+    let results = search(&grid);
+
+    Ok(results.len() as u64)
 }
 
 #[cfg(test)]
@@ -12,7 +97,7 @@ mod tests {
     #[test_log::test]
     fn test_process() -> miette::Result<()> {
         let input = include_str!("../test-input.txt");
-        assert_eq!(0, process(input)?);
+        assert_eq!(9, process(input)?);
         Ok(())
     }
 }
